b327a70007b5c6653913d43e54df92f905df8351
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2020-05-12 19:26:31 -0400

    Added native `_run`

--- lifefx/src/net/inet_lab/life/ui/NativeWrapper.java
+++ lifefx/src/net/inet_lab/life/ui/NativeWrapper.java
@@ -2,15 +2,25 @@ package net.inet_lab.life.ui;
 
 public class NativeWrapper {
     static {
-        System.loadLibrary("native");
+        System.loadLibrary("lifenative");
     }
 
-    public static boolean[] oneStep (boolean[] F, int X, int Y) {
+    public static boolean[] oneStep(boolean[] F, int X, int Y) {
         int x = 1 + 25;
-        boolean[] F1 = new boolean[X*Y];
-        _oneStep (X, Y, F, F1);
+        boolean[] F1 = new boolean[X * Y];
+        _oneStep(X, Y, F, F1);
         return F1;
     }
 
-    private static native void _oneStep (int X, int Y, boolean[] F, boolean[] F1);
+    private static native void _oneStep(int X, int Y, boolean[] F, boolean[] F1);
+
+    public interface runCallback {
+        int report(int iter, boolean[] F);
+    }
+
+    public static void run(int X, int Y, double tout, boolean[] F, runCallback cb) {
+        _run(X, Y, tout, F, cb);
+    }
+
+    private static native void _run(int X, int Y, double tout, boolean[] F, runCallback cb);
 }
