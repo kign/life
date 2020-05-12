76acf535385ca3ddf298f278ee17ab9ea45cc52a
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2020-05-12 19:21:59 -0400

    Renamed dylib, added javap information output

--- jni/build.bash
+++ jni/build.bash
@@ -3,6 +3,8 @@
 # if Java versions conflict
 javac -h . -d out ../lifefx/src/net/inet_lab/life/ui/NativeWrapper.java
 gcc -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin net_inet_lab_life_ui_NativeWrapper.c -o net_inet_lab_life_ui_NativeWrapper.o
-gcc -L../lib -dynamiclib -o libnative.dylib net_inet_lab_life_ui_NativeWrapper.o -lc -lliferun
+gcc -L../lib -dynamiclib -o liblifenative.dylib net_inet_lab_life_ui_NativeWrapper.o -lc -lliferun
 
-ls -l libnative.dylib
+javap -s -cp ../lifefx/out/production/lifefx net.inet_lab.life.ui.NativeWrapper.runCallback
+
+ls -l liblifenative.dylib
