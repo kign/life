af8f79571c8cdd9722e8bcc2a5b2c7870e87cb4c
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2020-05-13 18:52:01 -0400

    Added skip and delay (for now, constants)

--- lifefx/src/net/inet_lab/life/ui/Properties.java
+++ lifefx/src/net/inet_lab/life/ui/Properties.java
@@ -8,14 +8,23 @@ public class Properties {
 
     public final int nX;
     public final int nY;
+    public final double skip;
+    public final double delay;
+
     public Properties (final int nX, final int nY) {
         this.nX = nX;
         this.nY = nY;
+        this.skip = 0.1;
+        this.delay = 0.1;
     }
+
     public Properties(Preferences pref) {
         nX = pref.getInt(KEY_NX, 25);
         nY = pref.getInt(KEY_NY, 25);
+        this.skip = 0.1;
+        this.delay = 0.1;
     }
+
     public void save (Preferences pref) {
         pref.putInt(KEY_NX, nX);
         pref.putInt(KEY_NY, nY);
