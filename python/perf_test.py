d1b3f3ec8a26b0783d097f2a063c2dece01f953e
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2020-05-17 18:23:05 -0400

    Added density graph to perf_test.py

--- python/perf_test.py
+++ python/perf_test.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python3
 import os, sys, argparse, logging, random, re, time
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pprint import pprint
 
 sys.path.append(os.environ['HOME'] + "/git/inet-lab/shared/pylib3")
 from colorterm import add_coloring_to_emit_ansi
@@ -18,12 +22,15 @@ def get_args () :
     parser.add_argument('-d', '--density', type=float, help=f"density for randomized board generation (default = {default_density:0.4f})", default=default_density)
     parser.add_argument('-s', '--seed', help="Seed for randomized board generation (if omitted results will be different every time)")
     parser.add_argument('-i', '--iterations', type=int, help=f"Number of iterations (default = {default_iterations})", default=default_iterations)
+    parser.add_argument('--density-graph', action='store_true', help="Display density graph", dest='density_graph')
 
     parser.add_argument('size_a', nargs='+', help="board size", metavar="WIDTHxHEIGHT" )
 
     args = parser.parse_args ()
-    logging.basicConfig(format="%(asctime)s %(message)s",
+    logging.basicConfig(#format="%(asctime)s %(message)s",
                         level=getattr(logging, args.log_level.upper(), None))
+    logging.getLogger('matplotlib.font_manager').setLevel(logging.INFO)
+
     logging.StreamHandler.emit = add_coloring_to_emit_ansi(logging.StreamHandler.emit)
 
     if not (0 < args.density < 1) :
@@ -59,10 +66,22 @@ def main(args) :
 		for idx in range(X*Y):
 			Fin[idx] = random.random() < args.density
 
+		dens_a = [None] * args.iterations
+		def callback(liter, count, lhash, f, fin) :
+			if liter > 0 :
+				dens_a[liter - 1] = count/X/Y
+
 		t0 = time.time ()
-		n = life.run(X, Y, 1, args.iterations, Fin, Fout, None)
+		n = life.run(X, Y, 1, args.iterations, Fin, Fout,
+			             callback if args.density_graph else None)
 		t1 = time.time ()
 		print(f"[{X}x{Y}] Computed {n} iterations in {t1-t0:.3f} seconds @ {n/(t1-t0):.3f} fps")
+
+		if args.density_graph :
+			fig, ax = plt.subplots()
+			ax.plot(range(args.iterations), dens_a)
+			plt.show ()
+
 
 
 main (get_args())
