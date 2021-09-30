6a0bcb7ab6a994e69c7a2d804b788b589a929901
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2021-09-30 19:46:11 -0400

    Cleanup, update

--- python/perf_test.py
+++ python/perf_test.py
@@ -1,13 +1,7 @@
 #! /usr/bin/env python3
-import os, sys, argparse, logging, random, re, time
+import argparse, logging, random, re, time
 import matplotlib.pyplot as plt
-import numpy as np
-
-from pprint import pprint
-
-sys.path.append(os.environ['HOME'] + "/git/inet-lab/shared/pylib3")
-from colorterm import add_coloring_to_emit_ansi
-from genformatter import GenericFormatter
+from inetlab.cli.colorterm import add_coloring_to_emit_ansi
 
 def get_args () :
     default_log_level = "debug"
@@ -19,69 +13,74 @@ def get_args () :
                         choices=['debug', 'info', 'warning', 'error', 'critical'],
                         default=default_log_level)
 
-    parser.add_argument('-d', '--density', type=float, help=f"density for randomized board generation (default = {default_density:0.4f})", default=default_density)
-    parser.add_argument('-s', '--seed', help="Seed for randomized board generation (if omitted results will be different every time)")
-    parser.add_argument('-i', '--iterations', type=int, help=f"Number of iterations (default = {default_iterations})", default=default_iterations)
-    parser.add_argument('--density-graph', action='store_true', help="Display density graph", dest='density_graph')
+    parser.add_argument('-d', '--density', type=float,
+        help=f"density for randomized board generation (default = {default_density:0.4f})",
+        default=default_density)
+    parser.add_argument('-s', '--seed',
+        help="Seed for randomized board generation (if omitted results will be different every time)")
+    parser.add_argument('-i', '--iterations', type=int,
+        help=f"Number of iterations (default = {default_iterations})",
+        default=default_iterations)
+    parser.add_argument('--density-graph', action='store_true', help="Display density graph",
+        dest='density_graph')
 
     parser.add_argument('size_a', nargs='+', help="board size", metavar="WIDTHxHEIGHT" )
 
     args = parser.parse_args ()
-    logging.basicConfig(#format="%(asctime)s %(message)s",
+    logging.basicConfig(format="%(asctime)s.%(msecs)03d %(filename)s:%(lineno)d %(message)s",
                         level=getattr(logging, args.log_level.upper(), None))
     logging.getLogger('matplotlib.font_manager').setLevel(logging.INFO)
 
     logging.StreamHandler.emit = add_coloring_to_emit_ansi(logging.StreamHandler.emit)
 
     if not (0 < args.density < 1) :
-    	logging.error("Invalid dencity value %f, must be between 0 and 1", args.density)
-    	exit(1)
+        logging.error("Invalid dencity value %f, must be between 0 and 1", args.density)
+        exit(1)
 
     random.seed(args.seed)
 
     if not (0 < args.iterations < 1_000_000_000) :
-    	logging.error("Invalid number of iterations %d", args.iterations)
-    	exit(1)
+        logging.error("Invalid number of iterations %d", args.iterations)
+        exit(1)
 
     return args
 
 def main(args) :
-	import life
+    import conway_life as life
 
-	re_size = re.compile(r'^(\d+)[^0-9](\d+)$')
-	for size in args.size_a :
-		m = re_size.match(size)
-		if not m :
-			logging.error("Cannot parse size %s", size)
-			exit(1)
-		X = int(m.group(1))
-		Y = int(m.group(2))
-		if X < 2 or Y < 2 or X*Y > 1.0e9 :
-			logging.error("Invalid values %s", size)
-			exit(1)
+    re_size = re.compile(r'^(\d+)[^0-9](\d+)$')
+    for size in args.size_a :
+        m = re_size.match(size)
+        if not m :
+            logging.error("Cannot parse size %s", size)
+            exit(1)
+        X = int(m.group(1))
+        Y = int(m.group(2))
+        if X < 2 or Y < 2 or X * Y > 1.0e9 :
+            logging.error("Invalid values %s", size)
+            exit(1)
 
-		Fin = [False] * X*Y
-		Fout = [False] * X*Y
+        Fin = [False] * X * Y
+        Fout = [False] * X * Y
 
-		for idx in range(X*Y):
-			Fin[idx] = random.random() < args.density
+        for idx in range(X * Y):
+            Fin[idx] = random.random() < args.density
 
-		dens_a = [None] * args.iterations
-		def callback(liter, count, lhash, f, fin) :
-			if liter > 0 :
-				dens_a[liter - 1] = count/X/Y
+        dens_a = [None] * args.iterations
+        def callback(liter, count, lhash, f, fin) :
+            if liter > 0 :
+                dens_a[liter - 1] = count / X / Y
 
-		t0 = time.time ()
-		n = life.run(X, Y, 1, args.iterations, Fin, Fout,
-			             callback if args.density_graph else None)
-		t1 = time.time ()
-		print(f"[{X}x{Y}] Computed {n} iterations in {t1-t0:.3f} seconds @ {n/(t1-t0):.3f} fps")
+        t0 = time.time ()
+        n = life.run(X, Y, 1, args.iterations, Fin, Fout,
+                         callback if args.density_graph else None)
+        t1 = time.time ()
+        print(f"[{X}x{Y}] Computed {n} iterations in {t1-t0:.3f} seconds @ {n/(t1-t0):.3f} fps")
 
-		if args.density_graph :
-			fig, ax = plt.subplots()
-			ax.plot(range(args.iterations), dens_a)
-			plt.show ()
-
+        if args.density_graph :
+            fig, ax = plt.subplots()
+            ax.plot(range(args.iterations), dens_a)
+            plt.show ()
 
 
 main (get_args())
