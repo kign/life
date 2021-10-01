779c4f598d6c11b3d7dc518e6da0c14ad1dde6a1
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2021-09-30 20:40:41 -0400

    Another attempt to include ../lib in a portable way

--- pni/setup.py
+++ pni/setup.py
@@ -1,10 +1,17 @@
 from distutils.core import setup, Extension
-from os.path import exists
+from os import listdir, mkdir
+from shutil import copy
 
-prefix = '..' if exists('../lib/liferun.c') else '.'
+try :
+    mkdir('lib')
+    for f in listdir('../lib') :
+        if f.endswith('.c') or f.endswith('.h') :
+            copy('../lib/' + f, 'lib')
+except (FileExistsError, FileNotFoundError) :
+    pass
 
 mod_life = Extension('conway_life',
-      include_dirs = [prefix + '/lib'],
-      sources = ['python_life.c', prefix + '/lib/liferun.c', prefix + '/lib/lifestep.c'])
+      include_dirs = ['lib'],
+      sources = ['python_life.c', 'lib/liferun.c', 'lib/lifestep.c'])
 
 setup(ext_modules=[mod_life])
