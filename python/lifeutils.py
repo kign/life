965bb02ea1df854caf57439d687a50269ae849e9
Konstantin Ignatiev <kostya@inet-lab.net>
2021-10-12 08:43:17 -0400

    Reformatted

--- python/lifeutils.py
+++ python/lifeutils.py
@@ -1,9 +1,9 @@
 def readt(s) :
-	rows = s.strip().split("\n")
-	Y = len(rows)
-	X = len(rows[0])
-	assert all(len(row) == X for row in rows)
-	return X, Y, [x not in (' ', '.') for row in rows for x in row]
+    rows = s.strip().split("\n")
+    Y = len(rows)
+    X = len(rows[0])
+    assert all(len(row) == X for row in rows)
+    return X, Y, [x not in (' ', '.') for row in rows for x in row]
 
 def savet(X, Y, F) :
-	return "".join(''.join('x' if F[y*X+x] else '.' for x in range(X))+'\n' for y in range(Y))
+    return "".join(''.join('x' if F[y * X + x] else '.' for x in range(X)) + '\n' for y in range(Y))
